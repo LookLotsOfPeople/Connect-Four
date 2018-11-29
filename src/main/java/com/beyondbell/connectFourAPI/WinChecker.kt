a3f91538f37c2cf99bef0e088b6bd7eadac742ec
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2018-11-29 08:37:20 -0800

    Removed Identity Equalities

--- src/main/java/com/beyondbell/connectFourAPI/WinChecker.kt
+++ src/main/java/com/beyondbell/connectFourAPI/WinChecker.kt
@@ -3,17 +3,17 @@ package com.beyondbell.connectFourAPI
 fun getWinState(board: Board): WinState? {
 	//Checks Horizontal Wins
 	val horizontal = horizontalWin(board)
-	if (horizontal !== WinState.NoOne) {
+	if (horizontal != WinState.NoOne) {
 		return horizontal
 	}
 	//Checks Vertical Wins
 	val vertical = verticalWin(board)
-	if (vertical !== WinState.NoOne) {
+	if (vertical != WinState.NoOne) {
 		return vertical
 	}
 	//Checks Diagonal \ Wins
 	val leftRightDiagonal = leftRightDiagonalWin(board)
-	if (leftRightDiagonal !== WinState.NoOne) {
+	if (leftRightDiagonal != WinState.NoOne) {
 		return leftRightDiagonal
 	}
 	//Checks Diagonal / Win
@@ -22,7 +22,6 @@ fun getWinState(board: Board): WinState? {
 		return rightLeftDiagonal
 	}
 
-	// Checks for Full com.beyondbell.connectFourAPI.Board
 	return if (board.isFull()) {
 		WinState.NoOne
 	} else {
@@ -34,9 +33,9 @@ private fun horizontalWin(board: Board): WinState {
 	for (row in 0 until board.getRowCount()) {
 		for (column in 0 until board.getColumnCount()) {
 			if (board.getPiece(row, column) !== Piece.Empty
-					&& board.getPiece(row, column) === board.getPiece(row, (column + 3).toByte().toInt())
-					&& board.getPiece(row, column) === board.getPiece(row, (column + 3).toByte().toInt())
-					&& board.getPiece(row, column) === board.getPiece(row, (column + 3).toByte().toInt())) {
+					&& board.getPiece(row, column) == board.getPiece(row, (column + 3))
+					&& board.getPiece(row, column) == board.getPiece(row, (column + 3))
+					&& board.getPiece(row, column) == board.getPiece(row, (column + 3))) {
 				return board.getPiece(row, column).getWinner()
 			}
 		}
@@ -48,9 +47,9 @@ private fun verticalWin(board: Board): WinState {
 	for (row in 0 until board.getRowCount() - 3) {
 		for (column in 0 until board.getColumnCount()) {
 			if (board.getPiece(row, column) !== Piece.Empty
-					&& board.getPiece(row, column) === board.getPiece((row + 1).toByte().toInt(), column)
-					&& board.getPiece(row, column) === board.getPiece((row + 2).toByte().toInt(), column)
-					&& board.getPiece(row, column) === board.getPiece((row + 3).toByte().toInt(), column)) {
+					&& board.getPiece(row, column) == board.getPiece((row + 1), column)
+					&& board.getPiece(row, column) == board.getPiece((row + 2), column)
+					&& board.getPiece(row, column) == board.getPiece((row + 3), column)) {
 				return board.getPiece(row, column).getWinner()
 			}
 		}
@@ -62,9 +61,9 @@ private fun leftRightDiagonalWin(board: Board): WinState {
 	for (row in 0 until board.getRowCount() - 3) {
 		for (column in 0 until board.getColumnCount() - 3) {
 			if (board.getPiece(row, column) !== Piece.Empty
-					&& board.getPiece(row, column) === board.getPiece((row + 1).toByte().toInt(), (column + 1).toByte().toInt())
-					&& board.getPiece(row, column) === board.getPiece((row + 2).toByte().toInt(), (column + 2).toByte().toInt())
-					&& board.getPiece(row, column) === board.getPiece((row + 3).toByte().toInt(), (column + 3).toByte().toInt())) {
+					&& board.getPiece(row, column) == board.getPiece((row + 1), (column + 1))
+					&& board.getPiece(row, column) == board.getPiece((row + 2), (column + 2))
+					&& board.getPiece(row, column) == board.getPiece((row + 3), (column + 3))) {
 				return board.getPiece(row, column).getWinner()
 			}
 		}
@@ -74,11 +73,11 @@ private fun leftRightDiagonalWin(board: Board): WinState {
 
 private fun rightLeftDiagonalWin(board: Board): WinState {
 	for (row in 0 until board.getRowCount() - 3) {
-		for (column in (board.getColumnCount() - 1).toByte() downTo 4) {
+		for (column in board.getColumnCount() - 1 downTo 4) {
 			if (board.getPiece(row, column) !== Piece.Empty
-					&& board.getPiece(row, column) === board.getPiece((row + 1).toByte().toInt(), (column - 1).toByte().toInt())
-					&& board.getPiece(row, column) === board.getPiece((row + 2).toByte().toInt(), (column - 2).toByte().toInt())
-					&& board.getPiece(row, column) === board.getPiece((row + 3).toByte().toInt(), (column - 3).toByte().toInt())) {
+					&& board.getPiece(row, column) == board.getPiece((row + 1), (column - 1))
+					&& board.getPiece(row, column) == board.getPiece((row + 2), (column - 2))
+					&& board.getPiece(row, column) == board.getPiece((row + 3), (column - 3))) {
 				return board.getPiece(row, column).getWinner()
 			}
 		}
